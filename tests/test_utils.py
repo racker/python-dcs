3be66fa51ebfba0c4394398752497bcd74cbbf8a
Tomaz Muraus <tomaz@tomaz.me>
2012-08-30 14:19:38 -0700

    Add missing import.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+
 if sys.version_info < (2, 7):
     import unittest2 as unittest
 else:
